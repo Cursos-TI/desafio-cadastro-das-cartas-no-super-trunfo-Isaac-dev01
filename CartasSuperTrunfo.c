07d8c2c76f6b1f32d4c710a273076e14ae6420ca
Isaac-dev01 <isaacjosafa2024@gmail.com>
2025-02-01 00:44:06 +0000

    comecei o código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main() {
-
-    
+printf ("Desafio Cartas Super Trunfos - Brasil vs Argentina\n\n");
+printf ("Serão divididos 8 estados, identificados pelas letras de A a H.\n");
+printf ("Cada estado terá 4 cidades, numeradas de 1 a 4.\n\n");
+printf ("Estados e as cidades do Brasil:\n");
+printf ("1- São Paulo(A):\n");
+printf ("- São Paulo(A01)\n- Campinas(A02)\n- Sorocaba(A03)\n- Santos(A04)\n");
+printf ("2- Rio de Janeiro(B):\n");
+printf ("- Rio de Janeiro(B01)\n- Niterói(B02)\n- Petrópolis(B03)\n- Nova Iguaçu(B04)\n");
+printf ("3- Minas Gerais(C):\n");
+printf ("- Belo Horizonte(C01)\n- Uberlândia(C02)\n- Juiz de Fora(C03)\n- Contagem(C04)\n");
+printf ("4- Bahia(D):\n");
+printf ("- Salvador(D01)\n- Feira de Santana(D02)\n- Vitória da Conquista(D03)\n- Itabuna(D04)\n");
     return 0;
 }
